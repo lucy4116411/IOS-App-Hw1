ee1d7df5a46e1ad515de9929c189f27b4c277967
lucy4116411 <40395639+lucy4116411@users.noreply.github.com>
2019-03-22 02:50:02 +0800

    Add files via upload

---
+++ Hw01/Hw01/photoWallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  photoWallViewController.swift
+//  Hw01
+//
+//  Created by User20 on 2019/3/21.
+//  Copyright © 2019 ouo. All rights reserved.
+//
+
+import UIKit
+
+class photoWallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
